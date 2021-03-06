ace3addb0572651bb049f2924a4dea2e0785e111
Juno <DumSpiroSpero@163.com>
2021-03-06 23:35:46 +0800

    学生端（基本）完成

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 import click
+from flask_login import login_user
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:xsSZD0420@localhost:3306/selectsystem?utf8mb4'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:xsSZD0420@localhost:3306/selectsystem?utf8mb4'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
 app.config['SECRET_KEY'] = '123456'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['FLASK_DEBUG'] = True
@@ -11,14 +12,35 @@ db = SQLAlchemy(app)
 
 
 class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
+    __tablename__ = 'student'
+    sid = db.Column(db.String(10), primary_key=True)
+    spassword = db.Column(db.String(20))
 
 
 class Teacher(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20))
-    name = db.Column(db.String(20))
+    __tablename__ = 'teacher'
+    tid = db.Column(db.String(10), primary_key=True)
+    tpassword = db.Column(db.String(20))
+
+
+class Schoice(db.Model):
+    __tablename__ = 'schoice'
+    id = db.Column(db.String(10), db.ForeignKey('student.sid'), primary_key=True)
+    firstchoice = db.Column(db.String(10)) #####
+    secondchoice = db.Column(db.String(10))
+
+
+class Tchoice(db.Model):
+    __tablename = 'tchoice'
+    sid = db.Column(db.String(10), db.ForeignKey('student.sid'), primary_key=True)
+    tid = db.Column(db.String(10), db.ForeignKey('teacher.tid'), primary_key=True)
+    isfirst = db.Column(db.Integer)
+
+
+class Final(db.Model):
+    __tablename = 'final'
+    sid = db.Column(db.String(10), db.ForeignKey('student.sid'), primary_key=True)
+    tid = db.Column(db.String(10), db.ForeignKey('teacher.tid'), primary_key=True)
 
 
 @app.cli.command()
@@ -48,3 +70,31 @@ def index():
 def page_not_found(e):
     stu = Student.query.first()
     return render_template('404.html'), 404
+
+
+@app.route('/schoice', methods=['GET', 'POST'])
+def schoice():
+    if request.method == 'POST':
+        firstchoice = request.form.get('firstchoice')
+        secondchoice = request.form.get('secondchoice')
+        if not firstchoice or not secondchoice:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        s = Student.query.first() # 要改
+        sc = Schoice.query.filter_by(id='18120001').first()
+        sc.firstchoice = '12001'
+        sc.secondchoice = '12002'
+        # studentchoice = Schoice(id='18120001', firstchoice='12001', secondchoice='12002')
+        # db.session.add(studentchoice) # 更新
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+    stu = Student.query.first()
+    tea = Teacher.query.all()
+    return render_template('schoice.html', teachers=tea)
+
+
+@app.route('/shome')
+def stuhome():
+    tutor = Final.query.filter_by(sid='18120001').first()
+    return render_template('stuhome.html', tutor=tutor)
